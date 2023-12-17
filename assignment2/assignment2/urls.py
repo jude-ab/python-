f7d06de961eef4cdf565bcecbdb0d030b5bd0210
jude abouhajar <judeabouhajar@judes-MacBook-Air.local>
2023-12-17 00:39:26 +0000

    mehead

--- assignment2/assignment2/urls.py
+++ assignment2/assignment2/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from restnearme.views import user_login
+from django.conf import settings 
+from  django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,7 @@ urlpatterns = [
     path('', include('pwa.urls')),
     path('', user_login, name='user_login'),  
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
